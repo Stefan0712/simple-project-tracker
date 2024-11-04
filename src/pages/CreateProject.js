a735982fb7abb9d82228d1dde95fbca92b71dc2c
Vladulescu Stefan <s.vladulescu@gmail.com>
2024-11-04 19:07:49 +0200

    Added one form for adding a list and one for adding tasks when creating the project

--- src/pages/CreateProject.js
+++ src/pages/CreateProject.js
@@ -34,8 +34,6 @@ const CreateProject = () => {
     const [taskPriority, setTaskPriority] = useState('normal');
    
 
-
-
     const getColor = (color) =>{
         setStatusColor(color);
     }
@@ -58,8 +56,35 @@ const CreateProject = () => {
 
     //lists handlers
 
-    const addList = () =>{
-
+    const handleAddList = (e) =>{
+        e.preventDefault();
+        setLists((lists)=>[...lists, 
+            {
+                name: listName,
+                description: listDescription,
+                dueDate: listDueDate,
+                priority: listPriority
+            }
+        ]);
+        setListName('');
+        setListDescription('');
+        setListDueDate('');
+        setListPriority('normal');
+    }
+    const handleAddTask = (e) =>{
+        e.preventDefault();
+        setTasks((tasks)=>[...tasks, 
+            {
+                name: taskTitle,
+                description: taskDescription,
+                dueDate: taskDueDate,
+                priority: taskPriority
+            }
+        ]);
+        setTaskTitle('');
+        setTaskDescription('');
+        setTaskDueDate('');
+        setTaskPriority('normal');
     }
 
     return ( 
@@ -102,7 +127,54 @@ const CreateProject = () => {
                 </div>
                 <div className="second-half">
                     <h3>Add Lists</h3>
+                    <div className="new-list-form">
+                        <fieldset>
+                            <label>List Name</label>
+                            <input type="text" name="listName" id="listName" onChange={(e)=>setListName(e.target.value)} value={listName} />
+                        </fieldset>
+                        <fieldset>
+                            <label>List Description</label>
+                            <input type="text" name="listDescription" id="listDescription" onChange={(e)=>setListDescription(e.target.value)} value={listDescription} />
+                        </fieldset>
+                        <fieldset>
+                            <label>List Due Date</label>
+                            <input type="date" name="listDueDate" id="listDueDate" onChange={(e)=>setListDescription(e.target.value)} value={listDueDate} />
+                        </fieldset>
+                        <fieldset>
+                            <label>List Priority</label>
+                            <select name="listPriority" id="listPriority" onChange={(e)=>setListPriority(e.target.value)} value={listPriority}>
+                                <option value={'low'}>Low</option>
+                                <option value={'normal'}>Normal</option>
+                                <option value={'high'}>High</option>
+                            </select>
+                        </fieldset>
+                        <button className="add-list-button" onClick={handleAddList}>Add List</button>
+                    </div>
                     <h3>Add Tasks</h3>
+                    <div className="new-task-form">
+                        <fieldset>
+                            <label>Task Title</label>
+                            <input type="text" name="taskTitle" id="listName" onChange={(e)=>setListName(e.target.value)} value={listName} />
+                            
+                        </fieldset>
+                        <fieldset>
+                            <label>Task Description</label>
+                            <input type="text" name="taskDescription" id="taskDescription" onChange={(e)=>setTaskDescription(e.target.value)} value={taskDescription} />
+                        </fieldset>
+                        <fieldset>
+                            <label>Task Due Date</label>
+                            <input type="date" name="taskDueDate" id="taskDueDate" onChange={(e)=>setTaskDueDate(e.target.value)} value={taskDueDate} />
+                        </fieldset>
+                        <fieldset>
+                            <label>Task Priority</label>
+                            <select name="taskPriority" id="taskPriority" onChange={(e)=>setTaskPriority(e.target.value)} value={taskPriority}>
+                                <option value={'low'}>Low</option>
+                                <option value={'normal'}>Normal</option>
+                                <option value={'high'}>High</option>
+                            </select>
+                        </fieldset>
+                        <button className="add-task-button" onClick={handleAddTask}>Add Task</button>
+                    </div>
                     
                 </div>
             </form>
